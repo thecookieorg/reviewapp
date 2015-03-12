a205d679475c6b3055760ff34cbc17431262b4e8
Marko Manojlovic <marko@thecookie.org>
2015-03-12 13:42:48 -0700

    Add images with carrierwave, S3, and Figaro

--- Gemfile
+++ Gemfile
@@ -28,6 +28,12 @@ gem 'bootstrap-sass', '~> 3.3.3'
 gem 'rails_12factor', group: :production
 gem 'pg', group: :production
 
+gem 'carrierwave'
+
+gem "fog"
+
+gem "figaro"
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312195416) do
+ActiveRecord::Schema.define(version: 20150312202013) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150312195416) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image"
   end
 
 end
